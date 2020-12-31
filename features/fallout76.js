7ac73082b4ccc6b58bce01184b9f4c2e2a82066a
Casey Stark <starkca90@gmail.com>
2020-12-31 12:12:05 -0800

    Added 'help' card

--- features/fallout76.js
+++ features/fallout76.js
@@ -30,6 +30,44 @@ module.exports = function (controller) {
 
     })
 
+    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'help', ['message', 'direct_message'], async (bot, message) => {
+        // Get the fallout card template from the card library
+        let cardTemplate = new ACData.Template(cards['fallout_help'])
+
+        // Fill in the card template
+        // TODO: Possible multiple language support???
+        let cardPayload = cardTemplate.expand({
+            $root: {
+                "version": "1.0.0",
+                "title": "Fallout76 Silo Decoder",
+                "description": "I aid in decoding weekly nuclear silo codes in Fallout 76",
+                "supported_commands_title": "Supported Commands",
+                "commands": [
+                    {
+                        "key": "fallout",
+                        "value": "Displays the Entry Card"
+                    },
+                    {
+                        "key": "help",
+                        "value": "Displays this card"
+                    }
+                ],
+                "getting_started_title": "Getting Started",
+                "getting_started": "Request the entry card by sending me the *fallout* command",
+                "error_title": "Errors",
+                "error": "Errors are inevitable, if I run into run, I will try to help narrow down the issue, but no promises. Things to check include:",
+                "error_checks": "- The Correct Silo Keyword is entered (from the printer)\n - All 8 Code fragments from the right silo is entered properly\n - The Keyword and/or code fragments are from the same week"
+            }
+        })
+
+        // Send the card out to the requester
+        await bot.reply(message, {
+            text: "Cards are not supported on this platform yet",
+            attachments: cardPayload
+        })
+
+    })
+
     // User submitted the card back, time to decode
     controller.on('attachmentActions', async (bot, message) => {
         if (message.value.card === 'fallout') {
